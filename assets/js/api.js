fa65b6dae87416598c160e76dd8b55a63671a3be
joel de paula monteiro <37256731+joelpmonteiro@users.noreply.github.com>
2023-06-20 17:10:30 -0300

    commit new Layout
    commit new Layout

---
+++ assets/js/api.js
@@ -0,0 +1,26 @@
+const instance = axios.create({
+    baseURL: 'https://prototype.up.railway.app/',
+    timeout: 1000,
+    headers: { 'Content-Type': 'application/json' }
+});
+
+const createProject = async (projeto, resetForm) => {
+    try {
+        const { data, status } = await instance.post('projetos/salvar', { ...projeto })
+        if (status === 200) {
+            console.log(data)
+            alert('Projeto Criado com sucesso!');
+            resetForm.reset()
+        }
+    } catch (error) {
+        handlerError(error)
+    }
+}
+
+const handlerError = (error) => {
+    if (error.response.status === 400) {
+        alert('Erro ao criar o projeto, por que os dados não foram enviados corretamente!')
+    } else {
+        alert(msg, 'Erro ao criar o projeto!')
+    }
+}
